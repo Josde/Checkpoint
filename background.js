f678d7752a246bc32770e6223559470bd7e1850e
Josde <contact@josde.me>
2023-10-13 19:23:43 +0200

    add better url normalization

--- background.js
+++ background.js
@@ -2,12 +2,19 @@ browser.tabs.onUpdated.addListener(function (_id, _info, _tab) {
     if (_info.status === "loading" && _tab.url) {
       browser.storage.local.get({ blockedSites: [] }).then((data) => {
         const currentSite = new URL(_tab.url).hostname;
-
+        //TODO: this still does not detect a few cases, like urls starting with www.
+        // make a better normalize function and just reuse it i guess
+        // or maybe uri.js has one
         data.blockedSites.forEach(hostname => {
           if (currentSite === hostname) {
-            browser.tabs.update(_id, { url: `blocked.html?site=${currentSite}` }); 
-          }
-        });
+            browser.storage.session.get({ exceptions: [] }).then((data) => {
+            
+            if (data.exceptions == null || !data.exceptions.includes(_id)) {
+              browser.tabs.update(_id, { url: `blocked.html?site=${currentSite}` });
+               
+            } 
+          });
+      }});
     }
   )}
 });
